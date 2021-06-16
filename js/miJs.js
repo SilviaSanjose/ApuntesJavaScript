9c4805b4ff27d22763d54120b2dc36ec9f447855
Silvia Sanjose <sanjose.silvia@gmail.com>
2021-06-16 12:59:43 +0200

    actualización de dependencias

--- js/miJs.js
+++ js/miJs.js
@@ -1,251 +1,253 @@
-(()=> {
-    console.log("-*-*-*-*-COMIENZO miJs.js-*-*-*-*-")
+(() => {
+  console.log("-*-*-*-*-COMIENZO miJs.js-*-*-*-*-");
 })();
 
 //CARGAR PÁGINA CON DEMORA
-    //JQUERY
-      /*  $(function(){
+//JQUERY
+/*  $(function(){
             $("body").hide().fadeIn(3000);    //llamada a Jquery para que tarde 3seg en cargar el body
         })  */
-(function(){
-    document.getElementById('body').style.opacity = 0
-    setTimeout(()=>{
-        document.getElementById('body').style.opacity = 1;
-        document.getElementById('body').style.transition = 'opacity 2s ease-in-out';
-    }, 300);
+(function () {
+  document.getElementById("body").style.opacity = 0;
+  setTimeout(() => {
+    document.getElementById("body").style.opacity = 1;
+    document.getElementById("body").style.transition = "opacity 2s ease-in-out";
+  }, 300);
 })();
-    
+
 //******* */
 
 var camisetas, pantalones, total;
 camisetas = "5";
 pantalones = 5;
 
-total = Number(camisetas)+ pantalones;   //Number transforma un string a numero
+total = Number(camisetas) + pantalones; //Number transforma un string a numero
 
 var articulos = [56, 89, 20, 58];
 articulos.pop(); // elimina el último, 58
 
-
-function prueba(){
-    var lista = {"contacto": "Silvia", "resumen": "blablablab lablaa"}
-    console.log(lista)
+function prueba() {
+  var lista = { contacto: "Silvia", resumen: "blablablab lablaa" };
+  console.log(lista);
 }
 
 //ejemplo datos de un array
 var silvia = {
-    nombre: "silvia", 
-    colores: ["rojo", "verde"],
-    letras:  {primera: "A", segunda:"B"},
-    saludar: function () { console.log(`Hola ${this.nombre}`)  }
-    }
+  nombre: "silvia",
+  colores: ["rojo", "verde"],
+  letras: { primera: "A", segunda: "B" },
+  saludar: function () {
+    console.log(`Hola ${this.nombre}`);
+  },
+};
 
-silvia.nombre
-silvia.colores[1]
-silvia.letras.primera
-silvia.saludar()
+silvia.nombre;
+silvia.colores[1];
+silvia.letras.primera;
+silvia.saludar();
 
 //operador ternario
 var nom = "Silvia";
-var teLLamas = (nom === "Silvia") ? `Te llamas ${nom}` : "Quién eres?"
-console.log("Operador ternarios ", teLLamas)
-
+var teLLamas = nom === "Silvia" ? `Te llamas ${nom}` : "Quién eres?";
+console.log("Operador ternarios ", teLLamas);
 
 //Bucles
-console.log("*******Tipos bucle for******")
-var numeros = [10,20,30,40]
-var obj = {nombre: "me", apellido: "boo"}
-for(var i in numeros){      //for in debe llevar el nº del elemento. Se usa más para objetos
-    console.log(numeros[i])
+console.log("*******Tipos bucle for******");
+var numeros = [10, 20, 30, 40];
+var obj = { nombre: "me", apellido: "boo" };
+for (var i in numeros) {
+  //for in debe llevar el nº del elemento. Se usa más para objetos
+  console.log(numeros[i]);
 }
-for( var index in obj){
-    console.log(obj.nombre)   //imprime solo el valor de esa clave
-    console.log(`mi valor: ${obj[index]}`)   //imprime los valores por cada elemento
-    console.log(`mi clave: ${index}`)   //imprimre las claves
+for (var index in obj) {
+  console.log(obj.nombre); //imprime solo el valor de esa clave
+  console.log(`mi valor: ${obj[index]}`); //imprime los valores por cada elemento
+  console.log(`mi clave: ${index}`); //imprimre las claves
 }
-for(var i of numeros){    //for of muestra directamente el elemento. Para arrays
-    console.log(i)
+for (var i of numeros) {
+  //for of muestra directamente el elemento. Para arrays
+  console.log(i);
 }
 
 //Parametros rest
-console.log("*****Parámetros rest*****")
-function sumar (a, b, ...masArgumentos) {  
-    let res = a+b;
-    console.log(`argumento a: ${a} / argumento b: ${b}`)
-    console.log(`argumentos rest: ${masArgumentos}`)
- }
-sumar(2,3, 4, 1)
-
-function sumaArrow ( a, b, ...c){
-    let res2 = a +b;
-    c.forEach((n) =>{console.log(res2 += n)
-    })
+console.log("*****Parámetros rest*****");
+function sumar(a, b, ...masArgumentos) {
+  let res = a + b;
+  console.log(`argumento a: ${a} / argumento b: ${b}`);
+  console.log(`argumentos rest: ${masArgumentos}`);
 }
-sumaArrow(1,2,6)
+sumar(2, 3, 4, 1);
+
+function sumaArrow(a, b, ...c) {
+  let res2 = a + b;
+  c.forEach((n) => {
+    console.log((res2 += n));
+  });
+}
+sumaArrow(1, 2, 6);
 
 //ejemplo importaciones
-console.log("*****Export/Import***")
-import pordefecto, { usuario, suma20} from "./export.js";
-import {sumaFuncion, objetoExportado} from "./export.js";
+console.log("*****Export/Import***");
+import pordefecto, { usuario, suma20 } from "./export.js";
+import { sumaFuncion, objetoExportado } from "./export.js";
 
-console.log("importando mi nombre: ", usuario)
+console.log("importando mi nombre: ", usuario);
 
-let numero = 7; 
-let resImport = suma20 + numero   //suma20 importado
-console.log("Suma con importanción: ", resImport)
+let numero = 7;
+let resImport = suma20 + numero; //suma20 importado
+console.log("Suma con importanción: ", resImport);
 
-sumaFuncion(suma20, numero)
+sumaFuncion(suma20, numero);
 
-objetoExportado.saludar(usuario)
-console.log(objetoExportado.despedir)
+objetoExportado.saludar(usuario);
+console.log(objetoExportado.despedir);
 
 pordefecto();
 
 //Symbol
 
-(() =>{
-    console.log("***Symbol***")
-    let id1 = "hola"
-    let id2 = "hola"
-    console.log(id1 == id2)  //true
-    let id3 = Symbol("hola");
-    let id4 = Symbol("hola");
-    console.log(id3 == id4) //false
+(() => {
+  console.log("***Symbol***");
+  let id1 = "hola";
+  let id2 = "hola";
+  console.log(id1 == id2); //true
+  let id3 = Symbol("hola");
+  let id4 = Symbol("hola");
+  console.log(id3 == id4); //false
 
-    const NOMBRE = Symbol(); 
-    const persona = {  //creo objeto con Symbol
-        [NOMBRE]: "Silvi"
-    };
-    console.log(persona)  //>>  Symbol(): "Silvi"
+  const NOMBRE = Symbol();
+  const persona = {
+    //creo objeto con Symbol
+    [NOMBRE]: "Silvi",
+  };
+  console.log(persona); //>>  Symbol(): "Silvi"
 
-    persona.NOMBRE = "Silvi2"  //añado al objeto
-    console.log(persona)   //>> NOMBRA: "SIlvi2", Symbol(): "Silvi"
-    console.log(persona.NOMBRE)    //>> Silvi2
-    console.log(persona[NOMBRE])  //>> Silvi
+  persona.NOMBRE = "Silvi2"; //añado al objeto
+  console.log(persona); //>> NOMBRA: "SIlvi2", Symbol(): "Silvi"
+  console.log(persona.NOMBRE); //>> Silvi2
+  console.log(persona[NOMBRE]); //>> Silvi
 
-    const SALUDO = Symbol();  //creo nuevo Symbol
-    persona[SALUDO] = () => { console.log("Hola Symbol")}  //añado función Symbol al objeto
-    persona[SALUDO]();
+  const SALUDO = Symbol(); //creo nuevo Symbol
+  persona[SALUDO] = () => {
+    console.log("Hola Symbol");
+  }; //añado función Symbol al objeto
+  persona[SALUDO]();
 })();
 
-console.log("***********Generadores**********")
+console.log("***********Generadores**********");
 //generador, funcion para trabajar mas amigablemente con los elementos de un iterable; vuelve iterable una función
-  //* permite acceder al Symbol.iterator directamente.
-function* iterable(){
-    //yield es como un return que devuelve el valor de la propiedad next(), cuando no encuentra yield ha acabado.
-    yield "hola"; 
-    console.log("Hola consola");
-    yield "hola2";
-    console.log("Seguimos con los yiedl");
-    yield "hola3";
-    yield "hola4";
+//* permite acceder al Symbol.iterator directamente.
+function* iterable() {
+  //yield es como un return que devuelve el valor de la propiedad next(), cuando no encuentra yield ha acabado.
+  yield "hola";
+  console.log("Hola consola");
+  yield "hola2";
+  console.log("Seguimos con los yiedl");
+  yield "hola3";
+  yield "hola4";
 }
 let iterador = iterable();
 
-for (let y of iterador){
-    console.log(y)
+for (let y of iterador) {
+  console.log(y);
 }
 
 const arr = [...iterable()]; //añado los elemento iteables a un array
 console.log(arr);
 
-
-(()=> {
-})();
-    
-    
-
+(() => {})();
 
 //Callbacks
-console.log("******Callbacks******")
+console.log("******Callbacks******");
 
-function cuadradoCallback(value, callback){   //función recibe como parámetros un valor y una función llamada callback
-    setTimeout(() =>{
-        callback(value, value*value);       
-    },0 | Math.random()*100);
+function cuadradoCallback(value, callback) {
+  //función recibe como parámetros un valor y una función llamada callback
+  setTimeout(() => {
+    callback(value, value * value);
+  }, 0 | (Math.random() * 100));
 }
 //dentro de la función cuadradoCallback, se llama de nuevo a la función callback pasada como parámetro, que como esta definida abajo, recibe dos parámetros
 //valor y result que aqui corresponden con value y value*value
 
 //Llamada a la función: se pasa el valor,y una función anónima con dos parámetros(valor y result) que luego imprimirá
-cuadradoCallback(2,(valor, result)=>{    
-    console.log("Inicia Callback");
+cuadradoCallback(2, (valor, result) => {
+  console.log("Inicia Callback");
+  console.log(`Callback: ${valor}, ${result}`);
+  cuadradoCallback(3, (valor, result) => {
+    //se vuelve a llamar a la función... que no se ejecuta hasta que no termine la anterior, se puede producir Callback Hell*
     console.log(`Callback: ${valor}, ${result}`);
-    cuadradoCallback(3,(valor, result)=>{        //se vuelve a llamar a la función... que no se ejecuta hasta que no termine la anterior, se puede producir Callback Hell*
-        console.log(`Callback: ${valor}, ${result}`)
-    })
-})
+  });
+});
 
-//*Callback Hell son multiples llamadas una dentro de otra, que es mejor evitar ya que el código puede ser muy confuso. Para ello promesas entre otras cosas. 
-
+//*Callback Hell son multiples llamadas una dentro de otra, que es mejor evitar ya que el código puede ser muy confuso. Para ello promesas entre otras cosas.
 
 //PROMESAS
 
-function cuadradoPromesas(value){
-    if (typeof value !== 'number') return Promise.reject(`Error!! ${value} no es un número`)
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve({  //creamos objeto con value y result como claves
-            value, 
-            result: value * value
-        }); 
-    }, 1000 | Math.random() *100);
-});  
-} 
+function cuadradoPromesas(value) {
+  if (typeof value !== "number")
+    return Promise.reject(`Error!! ${value} no es un número`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        //creamos objeto con value y result como claves
+        value,
+        result: value * value,
+      });
+    }, 1000 | (Math.random() * 100));
+  });
+}
 
 cuadradoPromesas(0)
-    .then((obj)=>{   //recibe el resolve, en este caso nuestro objeto
-        console.log("***Inicio Promise****")
-        console.log(obj)
-        console.log(`Recibo el valor ${obj.value} y mi resultado ${obj.result}`);
-        return cuadradoPromesas(1);  //devuelve una llamada a la función con parámetro 1
-    })
-    .then(obj => {
-        console.log(`Promesa con 1, valor ${obj.value} y resultado ${obj.result}`);
-        return cuadradoPromesas(2)
-    })
-    .then(obj => {
-        console.log(`Promesa con 2, valor ${obj.value} y resultado ${obj.result}`);
-        return console.log("**Fin promesa***")
-    })
-    .catch(err => console.error(err));
-
+  .then((obj) => {
+    //recibe el resolve, en este caso nuestro objeto
+    console.log("***Inicio Promise****");
+    console.log(obj);
+    console.log(`Recibo el valor ${obj.value} y mi resultado ${obj.result}`);
+    return cuadradoPromesas(1); //devuelve una llamada a la función con parámetro 1
+  })
+  .then((obj) => {
+    console.log(`Promesa con 1, valor ${obj.value} y resultado ${obj.result}`);
+    return cuadradoPromesas(2);
+  })
+  .then((obj) => {
+    console.log(`Promesa con 2, valor ${obj.value} y resultado ${obj.result}`);
+    return console.log("**Fin promesa***");
+  })
+  .catch((err) => console.error(err));
 
 ///ASYNC/AWAIT
 
-async function asincrona(){
-        try {
-            console.log("****Incio async function****");
-            let obj= await cuadradoPromesas(0);
-            console.log(`Async Function 0: ${obj.value},  ${obj.result}`);
-            obj= await cuadradoPromesas(1);
-            console.log(`Async Function 1: ${obj.value},  ${obj.result}`);
-            obj= await cuadradoPromesas(2);
-            console.log(`Async Function 2: ${obj.value},  ${obj.result}`);
-        } catch (error) { 
-            console.error(error);
-         } 
+async function asincrona() {
+  try {
+    console.log("****Incio async function****");
+    let obj = await cuadradoPromesas(0);
+    console.log(`Async Function 0: ${obj.value},  ${obj.result}`);
+    obj = await cuadradoPromesas(1);
+    console.log(`Async Function 1: ${obj.value},  ${obj.result}`);
+    obj = await cuadradoPromesas(2);
+    console.log(`Async Function 2: ${obj.value},  ${obj.result}`);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // En caso de función anónima:
-const asincrona2 = async () =>{
-    try {
-        console.log("****Incio async function****");
-        let obj= await cuadradoPromesas(3);
-        console.log(`Async Function 3: ${obj.value},  ${obj.result}`);
+const asincrona2 = async () => {
+  try {
+    console.log("****Incio async function****");
+    let obj = await cuadradoPromesas(3);
+    console.log(`Async Function 3: ${obj.value},  ${obj.result}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-    } catch (error) { 
-        console.error(error);
-     } 
-} 
-
-(function(){    //añado setTimeout en vez de llamada directamente a asincrona() para que se muestre después de las promesas en consola
-    setTimeout(()=>{
-        asincrona()
-    }, 4000);
-    setTimeout(()=>{
-        asincrona2()
-    }, 8000);
+(function () {
+  //añado setTimeout en vez de llamada directamente a asincrona() para que se muestre después de las promesas en consola
+  setTimeout(() => {
+    asincrona();
+  }, 4000);
+  setTimeout(() => {
+    asincrona2();
+  }, 8000);
 })();
-
-
--- juegos/source/memory.js
+++ juegos/source/memory.js
@@ -1,112 +1,113 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const cardArray = [
+    {
+      name: "boostrap",
+      img: "img/bootstrap.png",
+    },
+    {
+      name: "boostrap",
+      img: "img/bootstrap.png",
+    },
+    {
+      name: "css",
+      img: "img/css3.png",
+    },
+    {
+      name: "css",
+      img: "img/css3.png",
+    },
+    {
+      name: "github",
+      img: "img/github_badge.png",
+    },
+    {
+      name: "github",
+      img: "img/github_badge.png",
+    },
+    {
+      name: "html",
+      img: "img/html5.png",
+    },
+    {
+      name: "html",
+      img: "img/html5.png",
+    },
+    {
+      name: "js",
+      img: "img/javascript_1.png",
+    },
+    {
+      name: "js",
+      img: "img/javascript_1.png",
+    },
+    {
+      name: "code",
+      img: "img/codepen.png",
+    },
+    {
+      name: "code",
+      img: "img/codepen.png",
+    },
+  ];
 
-    const cardArray = [
-        {
-            name: 'boostrap',
-            img: 'img/bootstrap.png'
-        },
-        {
-            name: 'boostrap',
-            img: 'img/bootstrap.png'
-        },
-        {
-            name: 'css',
-            img: 'img/css3.png'
-        },
-        {
-            name: 'css',
-            img: 'img/css3.png'
-        },
-        {
-            name: 'github',
-            img: 'img/github_badge.png'
-        },
-        {
-            name: 'github',
-            img: 'img/github_badge.png'
-        },
-        {
-            name: 'html',
-            img: 'img/html5.png'
-        },
-        {
-            name: 'html',
-            img: 'img/html5.png'
-        },
-        {
-            name: 'js',
-            img: 'img/javascript_1.png'
-        },
-        {
-            name: 'js',
-            img: 'img/javascript_1.png'
-        },{
-            name: 'code',
-            img: 'img/codepen.png'
-        },{
-            name: 'code',
-            img: 'img/codepen.png'
-        }
-    ]
+  cardArray.sort(() => 0.5 - Math.random());
+  const grid = document.querySelector(".grid");
+  const resultDisplay = document.querySelector("#result");
+  var cardsChosen = [];
+  var cardsChosenId = [];
+  var cardsWon = [];
 
+  function createBoard() {
+    for (var i = 0; i < cardArray.length; i++) {
+      var card = document.createElement("img");
+      card.setAttribute("src", "img/blank.png");
+      card.setAttribute("data-id", i);
+      card.addEventListener("click", flipCard);
+      grid.appendChild(card);
+    }
+  }
 
-    cardArray.sort(()=> 0.5 - Math.random())
-    const grid = document.querySelector('.grid');
-    const resultDisplay = document.querySelector('#result')
-    var cardsChosen = []
-    var cardsChosenId = []
-    var cardsWon = []
-
-    function createBoard(){
-        for(var i = 0; i< cardArray.length; i++){
-            var card = document.createElement('img');
-            card.setAttribute('src', 'img/blank.png');
-            card.setAttribute('data-id', i);   
-            card.addEventListener('click', flipCard);
-            grid.appendChild(card)
-        }
+  //check matches
+  function checkForMatch() {
+    var cards = document.querySelectorAll("img");
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+    if (optionOneId == optionTwoId) {
+      //same card
+      cards[optionOneId].setAttribute("src", "img/blank.png");
+      cards[optionTwoId].setAttribute("src", "img/blank.png");
+    } else if (cardsChosen[0] === cardsChosen[1]) {
+      //match
+      cards[optionOneId].setAttribute("src", cardArray[optionOneId].img);
+      cards[optionTwoId].setAttribute("src", cardArray[optionTwoId].img);
+      cards[optionOneId].removeEventListener("click", flipCard);
+      cards[optionTwoId].removeEventListener("click", flipCard);
+      cardsWon.push(cardsChosen);
+    } else {
+      //no match
+      cards[optionOneId].setAttribute("src", "img/blank.png");
+      cards[optionTwoId].setAttribute("src", "img/blank.png");
     }
 
-    //check matches
-    function checkForMatch(){
-        var cards =document.querySelectorAll('img')
-        const optionOneId = cardsChosenId[0]
-        const optionTwoId = cardsChosenId[1]
-        if(optionOneId == optionTwoId){  //same card
-            cards[optionOneId].setAttribute('src', 'img/blank.png')
-            cards[optionTwoId].setAttribute('src', 'img/blank.png')
-            
-        }else if(cardsChosen[0]=== cardsChosen[1]){  //match
-            cards[optionOneId].setAttribute('src', cardArray[optionOneId].img)
-            cards[optionTwoId].setAttribute('src', cardArray[optionTwoId].img)
-            cards[optionOneId].removeEventListener('click', flipCard)
-            cards[optionTwoId].removeEventListener('click', flipCard)
-            cardsWon.push(cardsChosen)
-        }else { //no match
-            cards[optionOneId].setAttribute('src', 'img/blank.png')
-            cards[optionTwoId].setAttribute('src', 'img/blank.png')
-        }
-        
-        cardsChosen = []
-        cardsChosenId= []
-        resultDisplay.textContent = cardsWon.length;
-        if ( cardsWon.length === cardArray.length/2){
-            document.getElementById('win-text').style.display = "block"
-            resultDisplay.textContent = "Felicidades"
-        }
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length === cardArray.length / 2) {
+      document.getElementById("win-text").style.display = "block";
+      resultDisplay.textContent = "Felicidades";
     }
+  }
 
-    //flip the card
-    function flipCard(){
-        var cardId = this.getAttribute('data-id');
-        cardsChosen.push(cardArray[cardId].name);
-        cardsChosenId.push(cardId);
-        this.setAttribute('src', cardArray[cardId].img);
-        if ( cardsChosen.length == 2){
-            setTimeout(checkForMatch, 400);
-        }
-
+  //flip the card
+  function flipCard() {
+    var cardId = this.getAttribute("data-id");
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute("src", cardArray[cardId].img);
+    if (cardsChosen.length == 2) {
+      setTimeout(checkForMatch, 400);
     }
+  }
 
-    createBoard();
-})
+  createBoard();
+});
